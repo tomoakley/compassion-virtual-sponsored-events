cbd3877347e12260bccf9cd852e56d1c4b5c3c59
Paurakh Sharma Humagain <paurakh011@gmail.com>
2022-10-15 13:49:31 +0100

    refactor: Fix image issue

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
